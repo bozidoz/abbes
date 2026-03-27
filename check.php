4573a439784b2b4e930afecd78e0e87bde1d0166
bozidoz <bozidi1993@gmail.com>
2026-03-27 02:39:15 +0100

    Update and rename index.php to check.php

---
+++ check.php
@@ -0,0 +1,13 @@
+{
+  "result": {
+    "newApkUrl": "",
+    "newVersionName": "",
+    "shouldUpdateGooglePlayVersion": false,
+    "forceAutoUpdate": false,
+    "stagedRolloutDays": 0,
+    "isActivated": true,
+    "deviceName": "TV",
+    "account": "andy@hax.net",
+    "responseToken": 79552
+  }
+}
