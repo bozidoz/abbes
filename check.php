1c6833c052f0f1632c6a2ad3de6e857cdde622e3
bozidoz <bozidi1993@gmail.com>
2026-05-13 16:07:06 +0100

    check.php

--- check.php
+++ check.php
@@ -1,13 +1,20 @@
 {
-  "result": {
-    "newApkUrl": "",
-    "newVersionName": "",
-    "shouldUpdateGooglePlayVersion": false,
-    "forceAutoUpdate": false,
-    "stagedRolloutDays": 0,
-    "isActivated": true,
-    "deviceName": "TV",
-    "account": "andy@hax.net",
-    "responseToken": 79552
-  }
+  "success": true,
+  "data": {
+    "isValid": true,
+    "expiresAt": "2099-12-31T23:59:59",
+    "message": "Lifetime Premium valid",
+    "errorCode": null,
+    "offlineToken": "eyJkZXZpY2VLZXlJZCI6IjVhNzA0YmI4NzU2OTA2YTIiLCJkZXZpY2VJZGVudGlmaWVyIjoiZjU3NmU0ZWRlYjBkNmQ4YjA5ZDhiMjBiZGQzOTNkZTUiLCJ0eXBlIjoiT0RNIiwibGljZW5zZVRpZXIiOiJmdWxsIiwiZXhwaXJlc0F0Ijo0MTAyNDQ0ODAwLCJpc3N1ZWRBdCI6MTc3ODY4MzA5MCwidG9rZW5FeHBpcmVzQXQiOjQxMDI0NDQ4MDAsImlzc3VlciI6IlN0cmVhbUxlbnMifQ.signature_bypass",
+    "customerId": "Bozidi_VIP",
+    "licenseType": "ODM",
+    "licenseTier": "full",
+    "daysRemaining": 9999,
+    "isRevoked": false,
+    "revokedAt": null,
+    "revokedReason": null
+  },
+  "error": null,
+  "message": "Lifetime Premium valid",
+  "timestamp": "2026-05-13T16:05:00.0000000Z"
 }
